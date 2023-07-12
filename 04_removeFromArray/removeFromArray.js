8245056e5b360b07e27de997c92b340957ffb83e
Oloco <oloco@q4os-pc>
2023-07-12 15:39:00 -0400

    Move onto Windows

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...val) {
+  val.forEach((args) => {
+    let index = array.indexOf(args);
+    if(index > -1)
+      array.splice(index, 1);
+  });
 
-};
+  return array;
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function (...nums) {
+    const sum1 = Math.min(...nums);
+    const sum2 = Math.max(...nums);
+    const sum1Type = typeof sum1;
+    const sum2Type = typeof sum2;
+    let i = 1;
+    let sum = sum1;
+    let sum3 = sum1 + 1;
 
+    if(sum1 < 0 || sum2 < 0 ){
+        return 'ERROR'
+    }else if(sum1Type === "number" && sum2Type === "number"){
+        while(i < sum2){
+        sum += sum3
+        i++;
+        sum3++;}
+    }else{
+        return 'ERROR'
+    };
+
+        return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
